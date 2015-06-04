9981badc77552a4f8c0713d23ec4f14cfd4a56af
Badri/Hippo <badrihippo@gmail.com>
2015-06-04 19:56:33 +0530

    Created AppDetail view and template

--- apps/urls.py
+++ apps/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from apps.views import AppList
+from apps.views import AppList, AppDetail
 urlpatterns = patterns('',
     url(r'^$', AppList.as_view(), name='applist'),
+    url(r'^(?P<slug>[-\w]+)/$', AppDetail.as_view(), name='app'),
 )
--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from apps.models import App
 
 class AppList(ListView):
+    model = App
+    
+class AppDetail(DetailView):
     model = App
