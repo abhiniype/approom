866787e61ceec732923c6dc921ef454f02530ebe
Badri/Hippo <badrihippo@gmail.com>
2015-06-04 19:29:13 +0530

    Added apps.urls to approom urls.py

--- approom/urls.py
+++ approom/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from approom.views import HomePageView
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'approom.views.home', name='home'),
+    url(r'^$', HomePageView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
+    
+    url(r'^apps/', include('apps.urls')),
     url(r"^account/", include("account.urls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
