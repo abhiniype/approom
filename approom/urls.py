77afa5d21db4a479553c9972c3997bb24853d668
Badri/Hippo <badrihippo@gmail.com>
2015-06-04 19:09:12 +0530

    Created homepage and dummy template

--- approom/urls.py
+++ approom/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'approom.views.home', name='home'),
+    url(r'^$', 'approom.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r"^account/", include("account.urls")),
     url(r'^admin/', include(admin.site.urls)),
